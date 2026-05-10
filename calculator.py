d89a29335b7e5b14c8f035dfb4ca4d2931517cd8
Navya <mnavya@gmail.com>
2026-05-10 08:59:26 +0530

    Updated calculator project

--- calculator.py
+++ calculator.py
@@ -1,30 +1,38 @@
-print("Simple Calculator")
+while True:
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+    print("\n===== SIMPLE CALCULATOR =====")
 
-print("Choose operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-choice = input("Enter choice (1/2/3/4): ")
+    print("\nChoose Operation")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
 
-if choice == '1':
-    print("Result:", num1 + num2)
+    choice = input("Enter choice (1/2/3/4): ")
 
-elif choice == '2':
-    print("Result:", num1 - num2)
+    if choice == '1':
+        print("Result =", num1 + num2)
 
-elif choice == '3':
-    print("Result:", num1 * num2)
+    elif choice == '2':
+        print("Result =", num1 - num2)
 
-elif choice == '4':
-    if num2 != 0:
-        print("Result:", num1 / num2)
+    elif choice == '3':
+        print("Result =", num1 * num2)
+
+    elif choice == '4':
+        if num2 != 0:
+            print("Result =", num1 / num2)
+        else:
+            print("Cannot divide by zero")
+
     else:
-        print("Cannot divide by zero")
+        print("Invalid choice")
 
-else:
-    print("Invalid choice")
+    again = input("\nDo you want to continue? (yes/no): ")
+
+    if again.lower() != "yes":
+        print("Calculator Closed")
+        break
